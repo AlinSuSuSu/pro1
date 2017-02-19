39ea6b91ac886f6921f8dc87f1ffa781dc535bb7
qun.su <alinsususu@163.com>
2017-02-19 18:20:44 +0800

    C1 E2-2 dynamic route

--- pro1.py
+++ pro1.py
@@ -1,15 +1,16 @@
 from flask import Flask
+from flask import current_app
 
 app = Flask(__name__)
 
 #路由是处理URL和函数之间关系的程序。
 @app.route('/')
 def hello_world():
-    return 'Hello missa!'
+    return 'Hello ss!'
 
-@app.route("/user/<name>")
+@app.route("/user/<name1>")
 def user(name1):
-    return 'hello,%s'% name1
+    return 'hello,%s'% current_app.name
 
 if __name__ == '__main__':
     app.run(debug = True )
